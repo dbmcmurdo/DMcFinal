94b447ef42bc9fb642583b782fafae9da5d35816
dbmcmurdo <dmcmurdo@northislandcollege.ca>
2018-12-03 12:23:19 -0800

    adds dynamic copyright and removes site info/meta data

--- wp-content/themes/twentysixteen/footer.php
+++ wp-content/themes/twentysixteen/footer.php
@@ -38,7 +38,19 @@
 				</nav><!-- .social-navigation -->
 			<?php endif; ?>
 
-			
+			<?php echo smallenvelop_copyright(); ?>
+			<div class="site-info">
+				<?php
+					/**
+					 * Fires before the twentysixteen footer text for footer customization.
+					 *
+					 * @since Twenty Sixteen 1.0
+					 */
+					do_action( 'twentysixteen_credits' );
+				?>
+				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
+</div><!-- .site-info -->	
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
 </div><!-- .site -->
